71aa500dc5721d5d3a6daeeb6fe29f3247e82951
Denys Dinkevych <dinkevich.hr@gmail.com>
2026-03-01 19:13:20 +0200

    feat: Remove unnecessary LanguageProvider
    The LanguageProvider was wrapping the entire application but was not being used. Removing it simplifies the app structure and removes unused context.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
+// Trigger new build
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Engineering from './pages/Engineering';
 import JobDetails from './pages/JobDetails';
 import Layout from './components/Layout';
 import CookieConsent from './components/CookieConsent';
-import { LanguageProvider } from './context/LanguageContext';
 
 export default function App() {
   return (
-    <LanguageProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/engineering" element={<Engineering />} />
-            <Route path="/job/:id" element={<JobDetails />} />
-          </Routes>
-        </Layout>
-        <CookieConsent />
-      </Router>
-    </LanguageProvider>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/engineering" element={<Engineering />} />
+          <Route path="/job/:id" element={<JobDetails />} />
+        </Routes>
+      </Layout>
+      <CookieConsent />
+    </Router>
   );
 }
